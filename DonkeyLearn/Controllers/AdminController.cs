804528bdb18a34d0b65b57d32567a33f96817156
Montes <angelmontes.ipn@gmail.com>
2024-05-09 22:16:40 -0600

    -

--- DonkeyLearn/Controllers/AdminController.cs
+++ DonkeyLearn/Controllers/AdminController.cs
@@ -1,11 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DonkeyLearn.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace DonkeyLearn.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult MenuAdmin()
+        string cadenaCon = "DATA SOURCE=.; INITIAL CATALOG=DONKEYLEARN; integrated security=true;" ;
+        public IActionResult MenuAdmin(DatosModel datos)
         {
+            string query = "SELECT * FROM GRUPO WHERE adm = " + datos.IdUsuario;
+            using (SqlConnection conn = new SqlConnection(cadenaCon))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    conn.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            return View();
+                        }
+                        else
+                        {
+                            return RedirectToAction("Grupo");
+                        }
+                    }
+                }
+            }
+           
+        }
+        [HttpGet]
+        public IActionResult Grupo() {
             return View();
         }
     }
