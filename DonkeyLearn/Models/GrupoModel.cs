759a3e9890bf2accf8e9744618fbe6ec72c691b4
CECyT13 <angemontes.ipn@gmail.com>
2024-05-13 09:43:07 -0600

    Clase para grupos

---
+++ DonkeyLearn/Models/GrupoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DonkeyLearn.Models
+{
+    public class GrupoModel
+    {
+        public int Clave { get; set; }
+        public int Clase { get; set;}
+        public string NomClase { get; set;}
+    }
+}
