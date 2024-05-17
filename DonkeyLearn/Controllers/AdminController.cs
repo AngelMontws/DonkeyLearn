c073ccd117b63820e55edc315bf445d2151a5a41
Montes <angelmontes.ipn@gmail.com>
2024-05-16 19:23:54 -0600

    Creo que va bien

--- DonkeyLearn/Controllers/AdminController.cs
+++ DonkeyLearn/Controllers/AdminController.cs
@@ -25,7 +25,11 @@ namespace DonkeyLearn.Controllers
                     {
                         if (dr.Read())
                         {
-                            return View(datos);
+                            HttpContext.Session.SetString("Grupo", dr["ID_Grupo"].ToString());  // Establecer el valor de la sesión "Grupo" aquí
+                            List<MateriaModel> materias = ObtenerMaterias(grupo);
+                            ViewData["Materias"] = materias;
+                            ViewData["Datos"] = datos;
+                            return View();
                         }
                         else
                         {
@@ -68,6 +72,54 @@ namespace DonkeyLearn.Controllers
                 TempData["Error"] = ex.ToString();
                 return RedirectToAction("Grupo");
             }
+        }
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Inicio", "Inicio");
+        }
+        public List<MateriaModel> ObtenerMaterias(string grupo)
+        {
+            grupo = HttpContext.Session.GetString("Grupo");  // Obtener el valor de la sesión "Grupo" aquí
+            List<MateriaModel> materias = new List<MateriaModel>();
+            string query = "SELECT ID_materia, Materia FROM UNI_APRE WHERE Grupo = @Grupo";
+            using (SqlConnection conn = new SqlConnection(cadenaCon))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Grupo", grupo);
+                    conn.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            materias.Add(new MateriaModel { ID = dr["ID_materia"].ToString(), Nombre = dr["Materia"].ToString() });
+                        }
+                    }
+                    conn.Close();
+                }
+            }
+            return materias;
+        }
+        [HttpPost]
+        public IActionResult EliminarMateria(string id, DatosModel datos)
+        {
+            
+            string query = "DELETE FROM UNI_APRE WHERE ID_materia = @ID_materia";
+            using (SqlConnection conn = new SqlConnection(cadenaCon))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@ID_materia", id);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+            TempData["Mensaje"] = "Materia eliminada";
+            datos.IdUsuario = HttpContext.Session.GetInt32("IdUsuario").GetValueOrDefault();
+            return RedirectToAction("MenuAdmin", datos);
         }
     }
 }
