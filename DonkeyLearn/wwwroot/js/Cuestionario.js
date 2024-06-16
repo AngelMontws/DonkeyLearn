2c21725bad66b202896567c3b51ec6537006c1eb
Markitos71 <marco.ruizant@gmail.com>
2024-06-16 15:01:03 -0600

    regresamos la logica pero el diseño se conserva

--- DonkeyLearn/wwwroot/js/Cuestionario.js
+++ DonkeyLearn/wwwroot/js/Cuestionario.js
@@ -1,58 +1,45 @@
-﻿// Inicializar el contador de preguntas
-let questionCount = 1; // Iniciando desde 1 porque ya hay una pregunta en el formulario inicial
-
-document.addEventListener("DOMContentLoaded", function () {
-    updateDeleteButtonState();
-});
-
-function addQuestion() {
+﻿function addQuestion() {
     var questionIndex = questionCount;
     var questionHtml = `
-        <div class="question">
-            <label for="pregunta${questionIndex + 1}">Pregunta ${questionIndex + 1}:</label>
-            <input type="text" id="pregunta${questionIndex + 1}" name="Preguntas[${questionIndex}].Pregunta" required />
-            ${getAnswersHtml(questionIndex)}
-        </div>
-    `;
-    var form = document.querySelector('form');
-    var buttonContainer = form.querySelector('.button-container');
-    buttonContainer.insertAdjacentHTML('beforebegin', questionHtml);
-    questionCount++;
-    updateDeleteButtonState();
-}
-
-function getAnswersHtml(questionIndex) {
+                <div>
+                    <label for="pregunta">Pregunta ${questionIndex + 1}:</label>
+                    <input type="text" id="pregunta" name="Preguntas[${questionIndex}].Pregunta" required />
+                </div>
+            `;
     var answersHtml = '';
     for (var i = 0; i < 4; i++) {
         var answerIndex = i + 1;
         answersHtml += `
-            <div class="answer">
-                <label for="inciso${questionIndex + 1}-${answerIndex}">Inciso ${answerIndex}:</label>
-                <input type="text" id="inciso${questionIndex + 1}-${answerIndex}" name="Preguntas[${questionIndex}].Respuestas[${i}].Respuesta" required />
-                <input type="checkbox" id="correcta${questionIndex + 1}-${answerIndex}" name="Preguntas[${questionIndex}].Respuestas[${i}].EsCorrecta" value="true" />
-                <label for="correcta${questionIndex + 1}-${answerIndex}">Correcta</label>
-            </div>
-        `;
+                    <div>
+                        <label for="inciso${answerIndex}">Inciso ${answerIndex}:</label>
+                        <input type="text" id="inciso${answerIndex}" name="Preguntas[${questionIndex}].Respuestas[${i}].Respuesta" required />
+                        <input type="checkbox" id="correcta${answerIndex}" name="Preguntas[${questionIndex}].Respuestas[${i}].EsCorrecta" value="true" />
+                        <label for="correcta${answerIndex}">Correcta</label>
+                    </div>
+                `;
     }
-    return answersHtml;
+    var questionContainer = document.createElement('div');
+    questionContainer.innerHTML = questionHtml + answersHtml;
+    var form = document.querySelector('form');
+    form.insertBefore(questionContainer, form.querySelector('button[type="button"]'));
+    updateDeleteButtonState();
+    questionCount++;
 }
-
 function deleteQuestion() {
-    var questionContainers = document.querySelectorAll('form > .question');
+    var questionContainers = document.querySelectorAll('form > div:not(:first-child)');
     if (questionContainers.length > 1) {
         var lastQuestionContainer = questionContainers[questionContainers.length - 1];
         lastQuestionContainer.remove();
-        questionCount--;
     } else {
         alert("No se puede eliminar la última pregunta.");
     }
+    questionCount--;
     updateDeleteButtonState();
 }
-
 function updateDeleteButtonState() {
-    var questionContainers = document.querySelectorAll('form > .question');
+    var questionContainers = document.querySelectorAll('form > div:not(:first-child)');
     var deleteButton = document.getElementById('deleteQuestionButton');
-    if (questionContainers.length > 1) {
+    if (questionContainers.length > 3) {
         deleteButton.disabled = false;
     } else {
         deleteButton.disabled = true;
