391625b1be5d4b4d45b8cd597e94a78098477adc
val <valeriacruzmtz629@gmail.com>
2024-06-17 10:12:49 -0600

    .

--- DonkeyLearn/Controllers/AdminController.cs
+++ DonkeyLearn/Controllers/AdminController.cs
@@ -17,7 +17,8 @@ namespace DonkeyLearn.Controllers
         //MARKITOS09\\SQLEXPRESS
         //------------------------------------Para Iniciar--------------------------------------------
         public string grupo;
-        string cadenaCon = "DATA SOURCE=R2D2\\SQLEXPRESS; INITIAL CATALOG=DONKEYLEARN; Trusted_Connection=yes;";
+        // string cadenaCon = "DATA SOURCE=R2D2\\SQLEXPRESS; INITIAL CATALOG=DONKEYLEARN; Trusted_Connection=yes;";
+        string cadenaCon = "DATA SOURCE=.; INITIAL CATALOG=DONKEYLEARN; integrated security=true;";
         public IActionResult MenuAdmin(DatosModel datos)
         {
             try
