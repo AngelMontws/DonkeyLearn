4b52fed4c884ce12e23bc14e48e5bbb4a62c18ae
Markitos71 <marco.ruizant@gmail.com>
2024-06-19 08:40:56 -0600

    asdf

--- DonkeyLearn/Controllers/AlumnoController.cs
+++ DonkeyLearn/Controllers/AlumnoController.cs
@@ -14,7 +14,7 @@ namespace DonkeyLearn.Controllers
 	public class AlumnoController : Controller
 	{
 
-		string cadenaCon = "DATA SOURCE=.; INITIAL CATALOG=DONKEYLEARN; integrated security=true;";
+		string cadenaCon = "DATA SOURCE=MARKITOS09\\SQLEXPRESS; INITIAL CATALOG=DONKEYLEARN; integrated security=true;";
 		//-----------------------------------INICIO-----------------------------------
 		[HttpGet]
 		public IActionResult Menu(DatosModel datos)
