59024f6603b5faba7c882a20f34e565bd47fdd23
CECyT 13 RFM <angelmontes.ipn@gmail.com>
2024-06-19 08:46:05 -0600

    standby

--- DonkeyLearn/Controllers/ProfeController.cs
+++ DonkeyLearn/Controllers/ProfeController.cs
@@ -493,26 +493,7 @@ namespace DonkeyLearn.Controllers
         /*[HttpPost]
         public IActionResult ReporteClase()
         {
-            string query = "SELECT Materia, Mat FROM UNI_APRE LEFT JOIN ENCARGADOS ON ID_materia = Mat WHERE Profesor = @Profesor";
-            List<SelectListItem> items = new List<SelectListItem>();
-
-            using (SqlConnection conn = new SqlConnection(cadenaCon))
-            {
-                using (SqlCommand cmd = new SqlCommand(query, conn))
-                {
-                    cmd.Parameters.AddWithValue("@Profesor", HttpContext.Session.GetInt32("IdUsuario"));
-                    conn.Open();
-                    using (SqlDataReader dr = cmd.ExecuteReader())
-                    {
-                        while (dr.Read())
-                        {
-                            items.Add(new SelectListItem { Value = dr["Mat"].ToString(), Text = dr["Materia"].ToString() });
-                        }
-                    }
-                }
-            }
-            ViewBag.Materias = items;
-            query = "SELECT "
+            
         }*/
     }
 }
